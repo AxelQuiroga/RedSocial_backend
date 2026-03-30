c53710156f73e4e2e87334bc007ca2aa2022e3bf
Seby <lokuraa18@hotmail.com>
2026-03-30 12:23:23 -0300

    front connected

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,21 @@ import userRoutes from "./interfaces/http/routes/user.routes.js";
 import { logger } from "./middlewares/logger.middleware.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 import postRoutes from "./interfaces/http/routes/post.routes.js";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(cors({
+  origin: FRONTEND_URL,
+  credentials: true,  
+}));
+ 
+
 
 app.use(express.json());
 app.use(logger);
